328338420003b15adf16e8e23b748600dde51103
Abhinandan-Dwivedi <168545625+Abhinandan-Dwivedi@users.noreply.github.com>
2025-07-03 10:04:45 +0000

    first commit

---
+++ variable.js
@@ -0,0 +1,13 @@
+// constant variable ( cann't changable )
+const x=  88;
+
+// scope is not defined  
+var v = "Kundan";
+
+//  commanly used for variables
+let l =  "hitesh sir";
+
+//  undefined variables;
+let un;
+
+console.table([x ,v, l ,un]);
