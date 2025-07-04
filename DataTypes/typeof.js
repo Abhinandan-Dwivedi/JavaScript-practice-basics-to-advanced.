b3807c9cf4747c01adbc394d4508f40b43043fb7
Abhinandan <dwabhi2006@gmail.com>
2025-07-04 10:06:08 +0530

    stack , heap allocation

--- DataTypes/typeof.js
+++ DataTypes/typeof.js
@@ -1,4 +1,4 @@
-// premitive data types : fundamental and Basic datatypes 
+// premitive data types : fundamental and Basic datatypes (these stores in stack , provides as copy of data)
 // string , number , Boolean , symbol , null , undefined , Bigint 
 
 let x  = 5004040404040400400n; // BigInt 
@@ -7,7 +7,7 @@ console.log(typeof x);
 let a = null;
 console.log(typeof a); // object 
 
-// Non premitive (Reference ) datatypes 
+// Non premitive (provided as Reference ) datatypes (these sotres in heap)
 // Array , function , object 
 let arr  = [ "kundan" , 5 , "dwivedi "];
 console.log(typeof arr);
@@ -22,3 +22,25 @@ let info  = {
     age : 20
 };
 console.log( typeof info)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack allocation of data
+let c  = " kundan";
+let d = c;
+d = "dwivedi";
+console.log(c);
+console.log(d)
+
+// heap allocation of data 
+let obj = {
+    name : "kd",
+    branch : "cse"
+}
+console.log(obj);
+
+let obj2 = obj;
+obj2.name = "dwivedi";
+console.log(obj); // here obj and obj2 both name changed because both has allcated same heap memory 
+console.log(obj2);
+
+
