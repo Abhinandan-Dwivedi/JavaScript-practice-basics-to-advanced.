14dde0e30250c9fdae7d1a5952cb42ef08970509
Abhinandan <dwabhi2006@gmail.com>
2025-07-05 23:32:58 +0530

    modifie object

--- 02Basics/03object.js
+++ 02Basics/03object.js
@@ -6,7 +6,8 @@ let myobject1 = {
     name : " kundan"
 }
 let myobject2 = {
-    name : "Abhinandan"
+    name : "Abhinandan",
+    email : "@gasil,com"
 }
 let myobject3 = {
     name :  "dwivedi"
@@ -16,6 +17,17 @@ myobject1.age = 20
 myobject1.id = 12311097;
  
 let myobject4 =  { myobject3 , myobject1 , myobject2};
-console.log(myobject4); // there are objects inside a object 
+// console.log(myobject4); // there are objects inside a object 
 
+let myobject5 =  Object.assign(myobject1 , myobject2 , myobject3); // if more than 1  obj has same key then last object key vlaue will be stored
+// console.log(myobject5);  
 
+let myobject6  =  { ...myobject1 ,...myobject2 , ...myobject3}; // spread method 
+//console.log(myobject6)  // if more than 1  obj has same key then last object key value will be stored
+
+let objtoarr=  Object.keys(myobject6); // make an array of kays
+console.log(objtoarr)
+console.log(Object.values(myobject6)); // gives an array of values
+
+// to check that a key is existing in an array or not 
+console.log(myobject6.hasOwnProperty("email")); // true 
